d15378afd16586f432a067fad5c8a88e1fe59ec0
yuanli <2865437316@qq.com>
2023-10-09 18:16:00 +0800

    fix: deploy error

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,34 +1,5 @@
-import NextAuth, {AuthOptions} from "next-auth";
-import Google from "next-auth/providers/google";
-import Github from "next-auth/providers/github";
-
-export const authOptions: AuthOptions = {
-    providers: [
-        Google({
-            clientId: process.env.AUTH_GOOGLE_ID,
-            clientSecret: process.env.AUTH_GOOGLE_SECRET,
-        }),
-        Github({
-            clientId: process.env.AUTH_GITHUB_ID,
-            clientSecret: process.env.AUTH_GITHUB_SECRET,
-        }),
-    ],
-    pages: {
-        signIn: '/login',
-    }
-}
-
-declare global {
-    namespace NodeJS {
-        export interface ProcessEnv {
-            NEXTAUTH_SECRET: string
-            AUTH_GITHUB_ID: string
-            AUTH_GITHUB_SECRET: string
-            AUTH_GOOGLE_ID: string
-            AUTH_GOOGLE_SECRET: string
-        }
-    }
-}
+import NextAuth from "next-auth";
+import {authOptions} from "@/app/api/auth/[...nextauth]/options";
 
 const handler = NextAuth(authOptions)
 
